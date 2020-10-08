256d3e27d3e6c826a5872a98625f8e7652975894
Yuval Azaria <yuval66550@gmail.com>
2020-10-08 16:34:43 +0300

    created user model with schema and export it

---
+++ server/models/user.js
@@ -0,0 +1,49 @@
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userRole = {
+  NORMAL: 0,
+  EDITOR: 1,
+  ADMIN: 2,
+};
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 255,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 1024,
+    },
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+exports.User = User;
