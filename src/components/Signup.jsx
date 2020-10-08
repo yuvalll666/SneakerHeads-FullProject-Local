c983bd234b5994b5c0e07b608dd4117d2beb543a
Yuval Azaria <yuval66550@gmail.com>
2020-10-08 19:10:46 +0300

    added Typography to signup header

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-
+//Components
 import Form from "./forms/Form";
 import MainContainer from "./forms/MainContainer";
 import Input from "./forms/Input";
 import PrimaryButton from "./forms/PrimaryButton";
+import Typography from "@material-ui/core/Typography";
+//Dependcies
 
 function Signup() {
   const { register, handleSubmit, errors } = useForm();
@@ -17,7 +19,9 @@ function Signup() {
 
   return (
     <MainContainer>
-      <h2 className="mt-4">Sign Up (step 1)</h2>
+      <Typography component="h2" variant="h5">
+        Sign Up (step 1)
+      </Typography>
       <Form className="mt-4">
         <Input ref={register} name="firstName" type="text" label="First Name" />
         <Input ref={register} name="lastName" type="text" label="Last Name" />
