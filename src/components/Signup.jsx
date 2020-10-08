c166d3e1a64e55d2104ad4796570b1b5a72bc51f
Yuval Azaria <yuval66550@gmail.com>
2020-10-08 19:05:43 +0300

    create PrimayButton component and add it to step 1

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import Form from "./forms/Form";
 import MainContainer from "./forms/MainContainer";
 import Input from "./forms/Input";
+import PrimaryButton from "./forms/PrimaryButton";
 
 function Signup() {
   const { register, handleSubmit, errors } = useForm();
@@ -18,19 +19,9 @@ function Signup() {
     <MainContainer>
       <h2 className="mt-4">Sign Up (step 1)</h2>
       <Form className="mt-4">
-        <Input
-          ref={register}
-          name="firstName"
-          type="text"
-          placeholder="First Name"
-        />
-        <Input
-          ref={register}
-          name="lastName"
-          type="text"
-          placeholder="Last Name"
-        />
-        <button type="submit">Next</button>
+        <Input ref={register} name="firstName" type="text" label="First Name" />
+        <Input ref={register} name="lastName" type="text" label="Last Name" />
+        <PrimaryButton type="submit">Next</PrimaryButton>
       </Form>
     </MainContainer>
   );
