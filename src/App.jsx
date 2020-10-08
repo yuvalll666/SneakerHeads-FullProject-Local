f728f20af7241993e876590c6ac5452eed096151
Yuval Azaria <yuval66550@gmail.com>
2020-10-08 17:32:11 +0300

    created Navbar componnent and added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light shadow">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Shop App
+        </Link> 
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/products">
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="#"
+                id="dropdown04"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Categories
+              </Link>
+              <div className="dropdown-menu" aria-labelledby="dropdown04">
+                <Link className="dropdown-item" to="#">
+                  Action
+                </Link>
+                <Link className="dropdown-item" to="#">
+                  Another action
+                </Link>
+                <Link className="dropdown-item" to="#">
+                  Something else here
+                </Link>
+              </div>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="signin">
+                  Signin
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="signup">
+                  Signup
+                </NavLink>
+              </li>
+            </React.Fragment>
+
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/product-page">
+                  <i className="fas fa-shopping-cart"></i>
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="dropdown04"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                ></Link>
+                <div className="dropdown-menu" aria-labelledby="dropdown04">
+                  <Link className="dropdown-item" to="/create-product">
+                    <i className="fas fa-caret-square-up"></i> Add Product
+                  </Link>
+                  <Link className="dropdown-item" to="/user-page">
+                    <i className="fas fa-cog"></i> Settings
+                  </Link>
+                  <Link className="dropdown-item" to="logout">
+                    <i className="fas fa-sign-out-alt"></i> Logout
+                  </Link>
+                </div>
+              </li>
+            </React.Fragment>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
