2772eba452ca1535ea95f2ba60b2b652cd423a3f
Yuval Azaria <yuval66550@gmail.com>
2020-10-11 03:07:40 +0300

    fix problem with valid inputs request

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,23 +2,24 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const { User, validateUser, userRole } = require("../models/user");
 const router = express.Router();
+const _ = require("lodash");
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
   const { error } = validateUser(req.body);
   if (error) {
     return res.status(400).send({ error: "Inputs validation problem" });
   }
   let user = await User.findOne({ email: req.body.email });
   if (user) {
-    return res.status(409).send({ error: "This email is taken" });
+    return res
+      .status(409)
+      .send({ error: "Email address is already rgistered" });
   }
   user = new User(req.body);
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  console.log("saved user");
-  await Promise.reject(new Error("whoooppsss"));
+  res.send(_.pick(user, ["_id", "firstName", "lastName", "email"]));
 });
 
 module.exports = router;
