5f0a346be254cdc87df4a553ac4cf0104fa52438
Yuval Azaria <yuval66550@gmail.com>
2020-10-12 15:32:20 +0300

    userService adding getCurrentUser function

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,17 @@
 import { apiUrl } from "../config.json";
 import http from "./httpService";
+import jwtDecode from "jwt-decode";
 
 const tokenKey = "token";
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
 
 export async function login(email, password) {
   const { data } = await http.post(`${apiUrl}/users/login`, {
