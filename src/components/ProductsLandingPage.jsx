114b69ecf85eeae9f0b6fc04d32a1d208512dad7
Yuval Azaria <yuval66550@gmail.com>
2020-10-14 17:59:18 +0300

    sending req to server to fetch products

---
+++ src/components/ProductsLandingPage.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import http from "../services/httpService";
+import { apiUrl } from "../config.json";
+
+function ProductsLandingPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    http.post(`${apiUrl}/products/getProducts`).then((response) => {
+      if (response.data.success) {
+        setProducts(response.data.products);
+
+        console.log(response.data.products);
+      } else {
+        alert("Faild to fetch products data");
+      }
+    });
+  }, []);
+  return <div></div>;
+}
+
+export default ProductsLandingPage;
