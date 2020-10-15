8f927e4eaa3524d04d880a53883d32586d04bbb3
Yuval Azaria <yuval66550@gmail.com>
2020-10-16 00:18:01 +0300

    create Checkbox component with brands names

---
+++ src/components/utils/CheckBox.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { CheckBox, Collapse } from "antd";
+const { Panel } = Collapse;
+
+const brands = [
+  {
+    _id: 1,
+    name: "NIKE",
+  },
+  {
+    _id: 2,
+    name: "ADIDAS",
+  },
+  {
+    _id: 3,
+    name: "VANS",
+  },
+  {
+    _id: 4,
+    name: "CONVERS",
+  },
+  {
+    _id: 5,
+    name: "NB",
+  },
+];
+
+function CheckBox() {
+  return (
+    <div>
+      <Collapse defaultActiveKey={["0"]}>
+        <Panel header key="1">
+          {brands.map((brand, index) => {
+            <React.Fragment>
+              <CheckBox key={index} onChange type="checkbox" checked />
+              <span>{brand.name}</span>
+            </React.Fragment>;
+          })}
+        </Panel>
+      </Collapse>
+    </div>
+  );
+}
+
+export default CheckBox;
